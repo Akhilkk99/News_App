f4c5f6c491384f6aa8e9f350f08ed70e2b8ab823
Ak hi <akhilkk4219@gmail.com>
2024-11-07 12:53:38 +0530

    finall

--- src/Firebase/setup.js
+++ src/Firebase/setup.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth,GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCaxfYXukCEF2ZZB-cUuxnreQLo07UJ61g",
-  authDomain: "news-app-de6df.firebaseapp.com",
-  projectId: "news-app-de6df",
-  storageBucket: "news-app-de6df.appspot.com",
-  messagingSenderId: "586747798692",
-  appId: "1:586747798692:web:d4ef7c6e9f79c1e15e7231"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth=getAuth(app)
-export const googleProvider= new GoogleAuthProvider(app)
-export const database = getFirestore(app)
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const database = getFirestore(app);
